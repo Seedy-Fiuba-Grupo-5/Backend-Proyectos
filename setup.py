101bc08d961836da95767f75c39875ffcd2833aa
Hugo <hlarrea@fi.uba.ar>
2021-04-24 21:09:26 -0300

    pruebas travis 4

--- setup.py
+++ setup.py
@@ -11,5 +11,4 @@ setup(name='Backend-Proyectos',
       author='Grupo 5',
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
      )
