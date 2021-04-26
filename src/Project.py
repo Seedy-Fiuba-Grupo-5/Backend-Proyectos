40a708721657f3e2571a878753766f422930df2a
diegobalestieri <diego.balestieri1998@gmail.com>
2021-04-26 20:31:04 -0300

    agrego funcion sin prueba

--- src/Project.py
+++ src/Project.py
@@ -1,6 +1,10 @@
 class Project:
     def __init__(self, name):
         self.name = name
+        self.isActive = False
 
     def get_name(self):
         return self.name
+
+    def activate_project(self):
+        self.isActive = True
