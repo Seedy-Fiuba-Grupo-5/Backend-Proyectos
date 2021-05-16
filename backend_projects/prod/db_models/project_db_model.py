19bb1c9d4e242161b8959fa2c6078a8fe3814058
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-16 12:19:31 -0300

    Refactor: Agrego ProjectDBModel

---
+++ backend_projects/prod/db_models/project_db_model.py
@@ -0,0 +1,22 @@
+from prod import db
+
+
+class ProjectDBModel(db.Model):
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    name = db.Column(db.String(128),
+                      unique=True,
+                      nullable=False)
+
+    def __init__(self,
+                 name):
+        self.name = name
+
+    # @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
