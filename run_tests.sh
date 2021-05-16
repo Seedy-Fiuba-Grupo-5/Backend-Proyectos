357fa6c9c3b214998ed4e441958fce9e187f12b6
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-16 13:06:10 -0300

    Fix: Corrijo docker-compose.yml y run_test.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[RUN_TESTS] waiting for postgres to start ..."
-docker-compose exec service_projects_web bash -c 'while ! nc -z service_users_db 5432; do sleep 1; done;'
+docker-compose exec service_projects_web bash -c 'while ! nc -z service_projects_db 5432; do sleep 1; done;'
 
 echo "[RUN_TESTS] Running tests ..."
 docker-compose exec service_projects_web pytest "backend_projects/dev/tests" --cov="backend_projects/prod" -p no:warnings
