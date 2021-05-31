f1409975b08dc5bfd827dd1e783840f6c7a6a56a
diegobalestieri <diego.balestieri1998@gmail.com>
2021-05-31 15:24:39 -0300

    fix: tests

--- backend_projects/dev/tests/test_project_api.py
+++ backend_projects/dev/tests/test_project_api.py
@@ -6,7 +6,8 @@ def test_db_with_only_project_id_1_name_Project_X_GET_id_1_should_return_just_th
     session.remove()
     test_database.drop_all()
     test_database.create_all()
-    session.add(ProjectDBModel(name='Project X'))
+    session.add(ProjectDBModel(name='Project X', description='description', hashtags='#prueba',
+        type='tipo',goal=1000,endDate='12/02/2021', location= 'Buenos Aires'))
     session.commit()
     client = test_app.test_client()
     response = client.get("/projects/1")
