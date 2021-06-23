8c09e22e6b9b02044021dd59e199a658ea1ab390
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-06-23 10:53:11 -0300

    fix: Add initial migration to db

--- prod/docker-entrypoint.sh
+++ prod/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-  
+
 until PGPASSWORD=$POSTGRES_PASSWORD psql $DATABASE_URL -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
-  
+
 >&2 echo "Postgres is up - executing command"
 
 # Ejecuta las migraciones en la (db)
@@ -12,7 +12,7 @@ flask db upgrade
 
 # Indica a Flask que levante un servidor
 # 0.0.0.0 : El servidor sera publicamente visible
-# ${PORT:-5000} : El puerto donde se bindea el server 
-# esta especificado por la variable de entorno PORT.  
+# ${PORT:-5000} : El puerto donde se bindea el server
+# esta especificado por la variable de entorno PORT.
 # PORT=5001, por defecto.
 flask run --host=0.0.0.0 --port=${PORT:-5000}
